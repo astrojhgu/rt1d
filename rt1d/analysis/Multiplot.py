Added a lot to appendix, sign errors from Thomas and Zaroubi fixed in code and paper.

rt1d/analysis/Multiplot.py
@@ -20,7 +20,11 @@ hspace = pl.rcParams['figure.subplot.hspace']
 left = pl.rcParams['figure.subplot.left']
 right = pl.rcParams['figure.subplot.right']
 bottom = pl.rcParams['figure.subplot.bottom']
-top = pl.rcParams['figure.subplot.top']    
+top = pl.rcParams['figure.subplot.top']
+dtop = 1 - top
+dbottom = bottom
+dright = 1 - right
+dleft = left
 
 def AxisConstructor(nr, nc, panel): 
     return pl.subplot(nr, nc, panel)
@@ -38,9 +42,23 @@ class multiplot:
         self.dims = dims
         self.padding = padding
         
-        if panel_size is None: self.panel_size = np.array([figsize[0], figsize[1]])
-        else: self.panel_size = np.array([panel_size[0] * figsize[0], panel_size[1] * figsize[1]])
-        
+        if panel_size is None: 
+            self.panel_size = np.array([figsize[0], figsize[1]])
+        else: 
+            
+            xsize = figsize[0]
+            if panel_size[0] > 1:
+                
+                for i in xrange(panel_size[0] - 1):
+                    xsize += (right - left) * figsize[0]
+            
+            ysize = figsize[1]    
+            if panel_size[1] > 1:
+                for i in xrange(panel_size[1] - 1):
+                    ysize += (top - bottom) * figsize[1]
+            
+            self.panel_size = np.array([xsize, ysize])
+                    
         if share_all and not useAxesGrid:
             pl.rcParams['figure.subplot.wspace'] = self.padding
             pl.rcParams['figure.subplot.hspace'] = self.padding        

rt1d/mods/Radiate.py
@@ -183,9 +183,9 @@ class Radiate:
        
         # Only solve helium rate equations if self.MultiSpeces = 1
         if self.MultiSpecies:
-            newHeII = Gamma_HeI * n_HeI - Beta_HeI * n_e * n_HeI + Beta_HeII * n_e * q[1] - \
+            newHeII = Gamma_HeI * n_HeI + Beta_HeI * n_e * n_HeI - Beta_HeII * n_e * q[1] - \
                       alpha_HeII * n_e * q[1] + alpha_HeIII * n_e * n_HeIII - xi_HeII * n_e * q[1]    
-            newHeIII = Gamma_HeII * n_HeII - Beta_HeII * n_e * n_HeII + alpha_HeIII * n_e * q[2]
+            newHeIII = Gamma_HeII * n_HeII + Beta_HeII * n_e * n_HeII - alpha_HeIII * n_e * q[2]
         else:
             newHeII = q[1]
             newHeIII = q[2]
@@ -695,23 +695,24 @@ class Radiate:
         for i, n in enumerate(nabs):
             cool += n * self.CollisionalIonizationCoolingCoefficient(T, i)
                 
+        # Cooling by recombinations
+        for i, n in enumerate(nion):
+            cool += n * self.RecombinationCoolingCoefficient(T, i)
+            
+        # Cooling by dielectronic recombination
+        cool += nion[2] * self.DielectricRecombinationCoolingCoefficient(T)
+                
+                
         # Cooling by collisional excitation
         if self.CollisionalExcitation:
             for i, n in enumerate(nabs):
                 cool += n * self.CollisionalExcitationCoolingCoefficient(T, nabs, nion, i)
         
-        # Cooling by recombinations
-        for i, n in enumerate(nion):
-            cool += n * self.RecombinationCoolingCoefficient(T, i)
-                        
-        # Cooling by dielectronic recombination
-        cool += nion[2] * self.DielectricRecombinationCoolingCoefficient(T)
-        
         # Compton cooling - from FK96
         if self.ComptonCooling:
             cool += 4. * k_B * (T - T_cmb) * (np.pi**2 / 15.) * (k_B * T_cmb / hbar / c)**3 * (k_B * T_cmb / m_e / c**2) * sigma_T * c
         
-        ## Cooling by free-free emission
+        # Cooling by free-free emission
         #cool += (nion[0] + nion[1] + 4. * nion[2]) * 1.42e-27 * 1.1 * np.sqrt(T) # Check on Gaunt factor        
                 
         cool *= n_e

scripts/AnalyzeTest2.py
@@ -47,12 +47,12 @@ a1 = ds.data[2].r[np.argmin(np.abs(ds.data[2].x_HI - 0.5))] / ds.pf['LengthUnits
 a2 = ds.data[20].r[np.argmin(np.abs(ds.data[20].x_HI - 0.5))] / ds.pf['LengthUnits']
 a3 = ds.data[100].r[np.argmin(np.abs(ds.data[100].x_HI - 0.5))] / ds.pf['LengthUnits']
 
-mp.grid[0].annotate('10', (a1 - 0.05, 0.5))
-mp.grid[0].annotate('100', (a2 - 0.07, 0.5))
-mp.grid[0].annotate('500', (a3 - 0.07, 0.5))
-mp.grid[1].annotate('10', (a1 - 0.05, 10**4.3))
-mp.grid[1].annotate('100', (a2 - 0.07, 10**4.2))
-mp.grid[1].annotate('500', (a3 - 0.07, 10**4.1))
+mp.grid[0].annotate('10', (a1 - 0.07, 0.5))
+mp.grid[0].annotate('100', (a2 - 0.1, 0.5))
+mp.grid[0].annotate('500', (a3 + 0.12, 0.5))
+mp.grid[1].annotate('10', (a1 - 0.07, 10**4.2))
+mp.grid[1].annotate('100', (a2 - 0.1, 10**4.12))
+mp.grid[1].annotate('500', (a3 + 0.12, 10**4.04))
 
 mp.fix_ticks()
 mp.grid[0].set_ylim(1e-5, 1.5)