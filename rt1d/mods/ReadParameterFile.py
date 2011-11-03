d8df937ce27cda644a84e48a2744403ebc1b44fd
Jordan Mirocha <mirochaj@gmail.com>
2011-11-03 14:34:34 -0600

    Changed default InitialRedshift = 10 for ProblemType = 4.

--- rt1d/mods/ReadParameterFile.py
+++ rt1d/mods/ReadParameterFile.py
@@ -142,7 +142,7 @@ def ProblemType(pt):
               "TimeUnits": s_per_myr, "CurrentTime": 0.0, "StopTime": 10.0, 
               "StartRadius": 0.01, "dtDataDump": 1, "DataDumpName": 'dd', \
               "Isothermal": 0, "MultiSpecies": 0, "SecondaryIonization": 1, "CosmologicalExpansion": 0, \
-              "DensityProfile": 1, "InitialRedshift": 20, "TemperatureProfile": 0, "InitialTemperature": 1e2, \
+              "DensityProfile": 1, "InitialRedshift": 10, "TemperatureProfile": 0, "InitialTemperature": 1e2, \
               "IonizationProfile": 1, "InitialHIIFraction": 1e-4, "SourceType": 3, "SourceLifetime": 1e10, \
               "SourceMass": 1e6, "SourceRadiativeEfficiency": 0.1, "SpectrumPowerLawIndex": 1.5
              }      
@@ -150,12 +150,11 @@ def ProblemType(pt):
     # X-ray source, hydrogen only, discrete spectrum         
     if pt == 4.1:
         pf = {"ProblemType": 4.1, "InterpolationMethod": 0, \
-              "ColumnDensityBinsHI": 500, "ColumnDensityBinsHeI": 0, "ColumnDensityBinsHeII": 0, 
-              "GridDimensions": 100, "LengthUnits": 100 * cm_per_kpc, \
+              "ColumnDensityBinsHI": 500, "GridDimensions": 100, "LengthUnits": 100 * cm_per_kpc, \
               "TimeUnits": s_per_myr, "CurrentTime": 0.0, "StopTime": 10.0, 
               "StartRadius": 0.01, "dtDataDump": 1, "DataDumpName": 'dd', \
               "Isothermal": 0, "MultiSpecies": 0, "SecondaryIonization": 1, "CosmologicalExpansion": 0, \
-              "DensityProfile": 1, "InitialRedshift": 20, "TemperatureProfile": 0, "InitialTemperature": 1e2, \
+              "DensityProfile": 1, "InitialRedshift": 10, "TemperatureProfile": 0, "InitialTemperature": 1e2, \
               "IonizationProfile": 1, "InitialHIIFraction": 1e-4, "SourceType": 3, "SourceLifetime": 1e10, \
               "DiscreteSpectrumSED": [500.], "DiscreteSpectrumRelLum": [1.], "DiscreteSpectrum": 1,\
               "SourceMass": 1e6, "SourceRadiativeEfficiency": 0.1
