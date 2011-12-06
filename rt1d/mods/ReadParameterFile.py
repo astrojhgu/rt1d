3b335c51c8781c878128e50f7e8d772e381c3ea8
Jordan Mirocha <mirochaj@gmail.com>
2011-12-05 22:35:52 -0700

    More default changing for problem 5.1.

--- rt1d/mods/ReadParameterFile.py
+++ rt1d/mods/ReadParameterFile.py
@@ -189,7 +189,10 @@ def ProblemType(pt):
     # X-ray source, helium included, discrete spectrum         
     if pt == 5.1:
         pf = {"ProblemType": 5.1, "InterpolationMethod": 0, \
-              "ColumnDensityBinsHI": 50, "ColumnDensityBinsHeI": 10, "ColumnDensityBinsHeII": 10, 
+              "ColumnDensityBinsHI": 50, "ColumnDensityBinsHeI": 20, "ColumnDensityBinsHeII": 20,
+              "HIColumnMin": 1e18, "HIColumnMax": 1e22, \
+              "HeIColumnMin": 1e17, "HeIColumnMax": 1e22, \
+              "HeIIColumnMin": 1e15, "HeIIColumnMax": 1e20, \
               "GridDimensions": 100, "LengthUnits": 100 * cm_per_kpc, \
               "TimeUnits": s_per_myr, "CurrentTime": 0.0, "StopTime": 10.0, 
               "StartRadius": 0.01, "dtDataDump": 1, "DataDumpName": 'dd', \
