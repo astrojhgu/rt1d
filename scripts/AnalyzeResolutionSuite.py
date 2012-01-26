ce16bab545d0ecb7a233079f137becdbd8b7d57a
Jordan Mirocha <mirochaj@gmail.com>
2012-01-26 10:48:04 -0700

    Added new RT06_2 resolution suite.

--- scripts/AnalyzeResolutionSuite.py
+++ scripts/AnalyzeResolutionSuite.py
@@ -34,7 +34,7 @@ pl.rcParams['figure.subplot.right'] = 0.88
 pl.rcParams['legend.fontsize'] = 10
 
 path = os.getcwd()
-pfs = ['dx100', 'dx200', 'dx400', 'dx800', 'dx1600', 'dx3200']#, 'dx6400']
+pfs = ['dx25', 'dx50', 'dx100', 'dx200', 'dx400', 'dx800']#, 'dx1600', 'dx3200']
 
 resolution = []
 meanerror = []
@@ -42,10 +42,9 @@ maxerror = []
 minerror = []
 
 # Highest resolution run
-ds = rta.Analyze("{0}/{1}.dat".format(path, 'dx6400'))
+ds = rta.Analyze("{0}/{1}.dat".format(path, 'dx800'))
 ds.ComputeIonizationFrontEvolution()
 ptype = ds.pf['ProblemType']
-ref_error = ds.rIF / ds.ranl
 
 # Loop over simulations
 mp = rta.multiplot(dims = (2, 1), panel_size = (1, 1), useAxesGrid = False)
@@ -58,29 +57,24 @@ for pf in pfs:
     if not os.path.exists("{0}/{1}".format(path, ds.pf["OutputDirectory"])): 
         continue
 
-    if ds.pf['GridDimensions'] == 100: color = 'cyan'
-    elif ds.pf['GridDimensions'] == 200: color = 'magenta'
-    elif ds.pf['GridDimensions'] == 400: color = 'red'
-    elif ds.pf['GridDimensions'] == 800: color = 'green'
-    elif ds.pf['GridDimensions'] == 1600: color = 'blue'
-    elif ds.pf['GridDimensions'] == 3200: color = 'k'
-    else: color = 'yellow' 
+    if ds.pf['GridDimensions'] == 25: color = 'cyan'
+    elif ds.pf['GridDimensions'] == 50: color = 'magenta'
+    elif ds.pf['GridDimensions'] == 100: color = 'red'
+    elif ds.pf['GridDimensions'] == 200: color = 'green'
+    elif ds.pf['GridDimensions'] == 400: color = 'blue'
+    elif ds.pf['GridDimensions'] == 800: color = 'k'
+    else: color = 'yellow'
                                                       
     ds.ComputeIonizationFrontEvolution()
         
-    if ds.pf['GridDimensions'] == 3200: 
+    if ds.pf['GridDimensions'] == 800: 
         mp.grid[0].plot(ds.t / ds.trec, ds.ranl, linestyle = '-', color = 'k', label = r'$r_{\mathrm{anl}}$')
         mp.grid[0].plot(ds.t / ds.trec, ds.rIF, color = 'k', ls = '--', label = r'$r_{\mathrm{num}}$')
-        mp.grid[1].plot(ds.t / ds.trec, ds.rIF / ds.ranl, color = 'k', ls = '-', label = r'$\Delta x = 1/3200$')
+        mp.grid[1].plot(ds.t / ds.trec, ds.rIF / ds.ranl, color = 'k', ls = '-', label = r'$\Delta x = 1/800$')
         
     else:
         mp.grid[1].plot(ds.t / ds.trec, ds.rIF / ds.ranl, color = color, ls = '-', label = r'$\Delta x = 1/{0}$'.format(int(ds.pf['GridDimensions'])))
-
-        resolution.append(ds.pf['GridDimensions'])
-        meanerror.append(np.abs(np.mean(ref_error - np.resize(ds.rIF / ds.ranl, len(ref_error)))))
-        maxerror.append(np.abs(np.max(ref_error - np.resize(ds.rIF / ds.ranl, len(ref_error)))))
-        minerror.append(np.abs(np.min(ref_error - np.resize(ds.rIF / ds.ranl, len(ref_error)))))
-
+        
 mp.grid[1].set_ylim(0.95, 1.05) 
 mp.fix_ticks()     
 mp.grid[0].set_xlim(0, max(ds.t / ds.trec))
@@ -98,30 +92,7 @@ mp.grid[0].legend(loc = 'lower right', frameon = False, ncol = 2)
 
 pl.draw()
 pl.savefig('RT06_{0}_IfrontEvolution.png'.format(ptype))
-
 pl.close()
 
-# Errors
-#f = fit(np.array(np.log10(resolution)), np.array(np.log10(meanerror)))
-#ax = pl.subplot(111)
-#ax.scatter(resolution, meanerror, color = 'k', label = 'Mean')
-#ax.scatter(resolution, maxerror, color = 'blue', label = 'Max')
-#ax.scatter(resolution, minerror, color = 'red', label = 'Min')
-#
-#ax.set_xlabel('Number of Grid Cells')
-#ax.set_ylabel('Error')
-#
-#ax.set_xscale('log')
-#ax.set_yscale('log')
-#
-#ax.loglog(resolution, 10**(f.pars[0] * np.log10(resolution) + f.pars[1]), color = 'k')
-#
-#ax.legend()
-#
-#pl.draw()
-#
-#raw_input('done')
-#
-#pl.savefig('RT06_{0}_Error_vs_Resolution.png'.format(int(sys.argv[1])))
 
 
