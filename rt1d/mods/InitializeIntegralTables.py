normalization in wrong direction.  Removed normalization for Psi since it is of order unity.

rt1d/mods/InitializeIntegralTables.py
@@ -583,16 +583,16 @@ class InitializeIntegralTables:
                                 
         # Otherwise, continuous spectrum                
         if self.pf.PhotonConserving:
-            integrand = lambda E: 1e10 * self.rs.Spectrum(E, t = t) * \
+            integrand = lambda E: 1e-10 * self.rs.Spectrum(E, t = t) * \
                 np.exp(-self.SpecificOpticalDepth(E, ncol)[0]) / \
                 (E * erg_per_ev)
         else:
-            integrand = lambda E: 1e10 * PhotoIonizationCrossSection(E, species) * \
+            integrand = lambda E: 1e-10 * PhotoIonizationCrossSection(E, species) * \
                 self.rs.Spectrum(E, t = t) * \
                 np.exp(-self.SpecificOpticalDepth(E, ncol)[0]) / \
                 (E * erg_per_ev)
             
-        return 1e-10 * integrate(integrand, max(E_th[species], self.rs.Emin), self.rs.Emax)[0]
+        return 1e10 * integrate(integrand, max(E_th[species], self.rs.Emin), self.rs.Emax)[0]
         
     def Psi(self, ncol, species = 0, donor_species = 0, xHII = 0.0, t = None):            
         """
@@ -601,14 +601,14 @@ class InitializeIntegralTables:
         
         # Otherwise, continuous spectrum    
         if self.pf.PhotonConserving:
-            integrand = lambda E: 1e20 * self.rs.Spectrum(E, t = t) * \
+            integrand = lambda E: self.rs.Spectrum(E, t = t) * \
                 np.exp(-self.SpecificOpticalDepth(E, ncol)[0])
         else:
-            integrand = lambda E: 1e20 * PhotoIonizationCrossSection(E, species) * \
+            integrand = lambda E: PhotoIonizationCrossSection(E, species) * \
                 self.rs.Spectrum(E, t = t) * \
                 np.exp(-self.SpecificOpticalDepth(E, ncol)[0])
         
-        return 1e-20 * integrate(integrand, max(E_th[species], self.rs.Emin), self.rs.Emax, epsrel = 1e-8)[0]
+        return integrate(integrand, max(E_th[species], self.rs.Emin), self.rs.Emax, epsrel = 1e-8)[0]
         
     def PhiWiggle(self, ncol, species = 0, donor_species = 0, xHII = 0.0, t = None):
         """