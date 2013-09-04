Fixed error in radiation source spectrum normalization - PL cutoff now applied in _Intensity (BHsrcs only).

rt1d/analyze/AnalyzeSimulation.py
@@ -101,7 +101,7 @@ class Simulation:
         
         return E, F, out
             
-    def StromgrenSphere(self, t, sol = 0, T0 = None):
+    def StromgrenSphere(self, t, sol=0, T0=None):
         """
         Classical analytic solution for expansion of an HII region in an 
         isothermal medium.  Given the time in seconds, will return the I-front 
@@ -123,6 +123,8 @@ class Simulation:
             self.trec = 1. / self.alpha_HII / self.data['dd0000']['h_1'][0] / n_H # s
             self.rs = (3. * self.Qdot \
                     / 4. / np.pi / self.alpha_HII / n_H**2)**(1. / 3.)  # cm
+        else:
+            raise NotImplementedError('')
         
         return self.rs * (1. - np.exp(-t / self.trec))**(1. / 3.) \
             + self.pf['start_radius']
@@ -341,7 +343,7 @@ class Simulation:
         
         return time, value
     
-    def IonizationRate(self, t = 1, absorber = 'h_1', color = 'k', ls = '-', 
+    def IonizationRate(self, t=1, absorber='h_1', color = 'k', ls = '-', 
         legend = True, plot_recomb = False, total_only = False, src = 0):
         """
         Plot total ionization rate, and lines for primary, secondary, and 

rt1d/physics/Hydrogen.py
@@ -11,8 +11,9 @@ Description: Container for hydrogen physics stuff.
 """
 
 import numpy as np
+from scipy.special import gamma
 import scipy.interpolate as interpolate
-from .Constants import A10, T_star, m_p, m_e, erg_per_ev, h, c
+from .Constants import A10, T_star, m_p, m_e, erg_per_ev, h, c, E_LyA, E_LL
 
 # Rate coefficients for spin de-excitation - from Zygelman originally
 
@@ -31,13 +32,23 @@ T_He = [1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0, 1000.0, 2000.0, 30
 kappa_He = [2.39e-10, 3.37e-10, 5.30e-10, 7.46e-10, 1.05e-9, 1.63e-9, 2.26e-9, 3.11e-9, 4.59e-9, 5.92e-9,
             7.15e-9, 7.71e-9, 8.17e-9, 8.32e-9, 8.37e-9, 8.29e-9, 8.11e-9]
 
+l_LyA = h * c / E_LyA / erg_per_ev
+
+g23 = gamma(2./3.)
+g13 = gamma(1./3.)
+
+c1 = 4. * np.pi / 3. / np.sqrt(3.) / g23
+c2 = 8. * np.pi / 3. / np.sqrt(3.) / g13
+
 class Hydrogen:
-    def __init__(self, cosm=None):
+    def __init__(self, cosm=None, approx_Salpha=1):
         if cosm is None:
             from .Cosmology import Cosmology
             self.cosm = Cosmology()
         else:
             self.cosm = cosm
+            
+        self.approx_S = approx_Salpha
         
         self.nmax = 23
         self.fbarII = 0.72
@@ -154,13 +165,32 @@ class Hydrogen:
         
         return 1.81e11 * self.Sa(Tk) * Ja / (1. + z)
         
-    def Sa(self, Tk):
-        #if Tk is None:
-        return 1.0
+    def tauGP(self, z, xHII=0.):
+        """ Gunn-Peterson optical depth. """
+        return 1.5 * self.cosm.nH(z) * (1. - xHII) * l_LyA**3 * 50e6 \
+            / self.cosm.HubbleParameter(z)
         
-        #alpha = 
-        #return np.exp(-1.79 * alpha)    
-    
+    def Sa(self, z=None, Tk=None, xHII=0.0):
+        """
+        Account for line profile effects.
+        """
+        
+        if self.approx_S == 1:
+            return 1.0
+        elif self.approx_S == 2:
+            return np.exp(-0.37 * np.sqrt(1. + z) * Tk**(-2./3.)) \
+                / (1. + 0.4 / Tk)
+        elif self.approx_S == 3:
+            gamma = 1. / self.tauGP(z, xHII=xHII) / (1. + 0.4 / Tk)
+            alpha = 0.717 * Tk**(-2./3.) * (1e-6 / gamma)**(1. / 3.)
+            return 1. - c1 * alpha - c2 * alpha**2 + 4. * alpha**3 / 3.
+        else:
+            raise NotImplementedError('')
+            
+    def ELyn(self, n):
+        """ Return energy of Lyman-n photon in eV. """
+        return E_LL * (1. - 1. / n**2)
+        
     def SpinTemp(self, z, Tk, Ja, nH, ne):
         """
         Returns spin temperature given:
@@ -174,7 +204,7 @@ class Hydrogen:
 
         x_c = self.CollisionalCouplingCoefficient(Tk, z, nH, ne)
         x_a = self.WouthuysenFieldCouplingCoefficient(z, Ja)
-        Tc = Tk # for now
+        Tc = Tk
                 
         return (1.0 + x_c + x_a) / \
             (self.cosm.TCMB(z)**-1. + x_c * Tk**-1. + x_a * Tc**-1.)
@@ -197,19 +227,5 @@ class Hydrogen:
             np.sqrt(0.15 * (1.0 + z) / self.cosm.OmegaMatterNow / self.cosm.h70**2 / 10.) * \
             (1.0 - self.cosm.TCMB(z) / Ts)
             
-    def AbsorptionSignal(self, z, Tk, Ja):
-        """
-        Signal assuming neutral medium.
-        """        
-        
-        nH = self.cosm.nH0 * (1. + z)**3
-        Ts = self.Ts(z, Tk, Ja, nH, 0.0)
-        
-        return 27. * \
-            (self.cosm.OmegaBaryonNow * self.cosm.h70**2 / 0.023) * \
-            np.sqrt(0.15 * (1.0 + z) / self.cosm.OmegaMatterNow / self.cosm.h70**2 / 10.) * \
-            (1.0 - self.cosm.TCMB(z) / Ts)
-            
-            
-            
+
             

rt1d/sources/BlackHoleSource.py
@@ -15,6 +15,7 @@ from scipy.integrate import quad
 from ..physics.Constants import *
 from .StellarSource import _Planck
 from ..util.SetDefaultParameterValues import BlackHoleParameters
+from ..physics.CrossSections import PhotoIonizationCrossSection as sigma_E
 
 sptypes = {'pl':0, 'mcd':1, 'qso':2}
 
@@ -60,6 +61,8 @@ class BlackHoleSource(object):
         self.T_in = self._DiskInnermostTemperature(self.M0)
         self.T_out = self._DiskTemperature(self.M0, self.r_out)
         self.Lbol = self.Luminosity(0.0)
+        
+        self.disk_history = {}
         
         if 2 in self.spec_pars['type']:
             self.fcol = self.spec_pars['fcol'][self.spec_pars['type'].index('mcd')]
@@ -147,6 +150,7 @@ class BlackHoleSource(object):
         References
         ----------
         Mitsuda et al. 1984, PASJ, 36, 741.
+        
         """         
         
         # If t > 0, re-compute mass, inner radius, and inner temperature
@@ -223,13 +227,20 @@ class BlackHoleSource(object):
         """
                         
         if self.type_by_name[i] == 'pl': 
-            return self._PowerLaw(E, i, t)    
+            Lnu = self._PowerLaw(E, i, t)    
         elif self.type_by_name[i] == 'mcd':
-            return self._MultiColorDisk(E, i, t)
+            Lnu = self._MultiColorDisk(E, i, t)
         elif self.type_by_name[i] == 'qso':
-            return self._QuasarTemplate(E, i, t)
+            Lnu = self._QuasarTemplate(E, i, t)
         else:
-            return 0.0
+            Lnu = 0.0
+            
+        if self.spec_pars['logN'][i] > 0:
+            Lnu *= np.exp(-10.**self.spec_pars['logN'][i] \
+                * (sigma_E(E, 0) + y * sigma_E(E, 1)))  
+        
+        return Lnu          
+            
             
     def _NormalizeSpectrum(self, t=0.):
         norms = np.zeros(self.N)
@@ -238,7 +249,7 @@ class BlackHoleSource(object):
             integral, err = quad(self._Intensity,
                 self.spec_pars['EminNorm'][i], self.spec_pars['EmaxNorm'][i], 
                 args=(i, t,))
-            
+                
             norms[i] = self.spec_pars['fraction'][i] * Lbol / integral
             
         return norms
@@ -248,8 +259,8 @@ class BlackHoleSource(object):
         Returns the bolometric luminosity of a source in units of erg/s.  
         For accreting black holes, the bolometric luminosity will increase 
         with time, hence the optional 't' and 'M' arguments.
-        """        
-        
+        """
+
         if not self.SourceOn(t):
             return 0.0
             

rt1d/sources/RadiationSource.py
@@ -79,7 +79,7 @@ class RadiationSource(object):
             self.src = DiffuseSource(self.pf, self.SourcePars, 
                 self.SpectrumPars)
         else:
-            raise ValueError('')
+            raise NotImplementedError('Unrecognized source_type')
                               
         # Number of spectral components
         self.N = len(self.SpectrumPars['type'])                  
@@ -382,10 +382,10 @@ class RadiationSource(object):
                 
         """       
                
-        emission = 0
+        emission = 0.0
         
         # Loop over spectral components
-        for j in xrange(self.src.N):
+        for j in xrange(self.N):
             if i is not None:
                 if j != i:
                     continue
@@ -393,15 +393,9 @@ class RadiationSource(object):
             if not (self.SpectrumPars['Emin'][j] <= E <= \
                     self.SpectrumPars['Emax'][j]):
                 continue        
-                    
-            tmp = self._normL[j] * \
-                self.src._Intensity(E, j, t) / self.Lbol0
-                
-            if self.SpectrumPars['logN'][j] > 0:
-                tmp *= np.exp(-10.**self.SpectrumPars['logN'][j] \
-                    * (sigma_E(E, 0) + y * sigma_E(E, 1)))
-                
-            emission += tmp 
+            
+            emission += self._normL[j] * \
+                self.src._Intensity(E, i=j, t=t) / self.Lbol0
             
         return emission
         